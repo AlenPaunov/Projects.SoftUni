fafbd127ab503693bc62a36f8cfc7410e6a22b73
Slavi Kapsalov <slavi.kapsalov@gmail.com>
2019-04-03 13:52:19 +0300

    Implement listing all projects with application status waiting

--- src/Web/ProjectsSoftuni.Web/Areas/Administration/Controllers/ProjectsController.cs
+++ src/Web/ProjectsSoftuni.Web/Areas/Administration/Controllers/ProjectsController.cs
@@ -1,8 +1,7 @@
-﻿namespace ProjectsSoftuni.Web.Areas.Administration.Controllers
-{
-    using System.Linq;
-    using System.Threading.Tasks;
+﻿using ProjectsSoftuni.Web.Models;
 
+namespace ProjectsSoftuni.Web.Areas.Administration.Controllers
+{
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.Rendering;
     using ProjectsSoftuni.Common;
@@ -10,6 +9,8 @@
     using ProjectsSoftuni.Services.Models.Projects;
     using ProjectsSoftuni.Web.Areas.Administration.ViewModels.Applications;
     using ProjectsSoftuni.Web.Areas.Administration.ViewModels.Projects;
+    using System.Linq;
+    using System.Threading.Tasks;
 
     public class ProjectsController : AdministrationController
     {
@@ -113,6 +114,74 @@
             }
 
             return this.View(project);
+        }
+
+        public async Task<IActionResult> ProjectsWithApplicationStatusWaiting(string sortOrder, string searchString, string currentFilter, int? pageIndex)
+        {
+            this.ViewData["NameSortParam"] = sortOrder == "name" ? "name_desc" : "name";
+            this.ViewData["OwnerSortParam"] = sortOrder == "owner" ? "owner_desc" : "owner";
+            this.ViewData["StatusSortParam"] = sortOrder == "status" ? "status_desc" : "status";
+            this.ViewData["CurrentFilter"] = searchString;
+            this.ViewData["CurrentSort"] = sortOrder;
+
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            this.ViewData["CurrentFilter"] = searchString;
+
+            var projects = await this.projectService.GetAllByApplicationStatusNameAsync<ProjectIndexViewModel>(GlobalConstants.WaitingApplicationStatus);
+
+            if (projects == null)
+            {
+                var controllerName = ControllerHelper.RemoveControllerFromStr(nameof(HomeController));
+                return this.RedirectToAction(nameof(HomeController.Index), controllerName);
+            }
+
+            if (!string.IsNullOrWhiteSpace(searchString))
+            {
+                projects = projects
+                     .Where(p => p.Name.ToLower().Contains(searchString.ToLower())
+                         || p.Owner.ToLower().Contains(searchString.ToLower()))
+                     .ToList();
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    projects = projects.OrderByDescending(p => p.Name).ToList();
+                    break;
+                case "name":
+                    projects = projects.OrderBy(p => p.Name).ToList();
+                    break;
+                case "owner_desc":
+                    projects = projects.OrderByDescending(p => p.Owner).ToList();
+                    break;
+                case "owner":
+                    projects = projects.OrderBy(p => p.Owner).ToList();
+                    break;
+                case "status_desc":
+                    projects = projects.OrderByDescending(p => p.StatusName).ToList();
+                    break;
+                case "status":
+                    projects = projects.OrderBy(p => p.StatusName).ToList();
+                    break;
+            }
+
+            var projectStatuses = this.projectStatusService.GetAllProjectStatuses();
+            this.ViewData[ProjectStatusesStr] = projectStatuses.Select(s => new SelectListItem
+            {
+                Value = s.Id.ToString(),
+                Text = s.Name,
+            });
+
+            int pageSize = 5;
+            return this.View(PaginatedList<ProjectIndexViewModel>.Create(projects, pageIndex ?? 1, pageSize));
         }
     }
 }
